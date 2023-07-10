d9bd795441ec4c73db98864db37f9738d797fdfe
won-yeongdeok <dudejr0608@gmail.com>
2023-07-11 06:58:14 +0900

    keep going

--- programmers/JOIN/없어진 기록 찾기.sql
+++ programmers/JOIN/없어진 기록 찾기.sql
@@ -3,4 +3,4 @@ SELECT b.animal_id,
 FROM   animal_ins a
        RIGHT JOIN animal_outs b
                ON a.animal_id = b.animal_id
-WHERE  a.animal_id IS NULL 
+WHERE  a.animal_id IS NULL;
--- programmers/JOIN/있었는데요 없었습니다.sql
+++ programmers/JOIN/있었는데요 없었습니다.sql
@@ -4,4 +4,4 @@ FROM   animal_ins a
        LEFT JOIN animal_outs b
               ON a.animal_id = b.animal_id
 WHERE  a.datetime > b.datetime
-ORDER  BY a.datetime
+ORDER  BY a.datetime;
