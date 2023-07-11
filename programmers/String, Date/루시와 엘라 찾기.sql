a7ec4504c6cd38797f146475911d75227bed02a4
won-yeongdeok <dudejr0608@gmail.com>
2023-07-12 06:43:48 +0900

    keep going

---
+++ programmers/String, Date/루시와 엘라 찾기.sql
@@ -0,0 +1,7 @@
+SELECT animal_id,
+       name,
+       sex_upon_intake
+FROM   animal_ins
+WHERE  name IN ( 'Lucy', 'Ella', 'Pickle', 'Rogan',
+                 'Sabrina', 'Mitty' )
+ORDER  BY animal_id ASC;
---
+++ programmers/String, Date/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM   car_rental_company_car
+WHERE  options LIKE '%네비게이션%'
+ORDER  BY car_id DESC; 
