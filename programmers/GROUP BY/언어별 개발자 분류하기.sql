15237abd2c040aa5702e7b658c5fbdb03253b6fc
won-yeongdeok <dudejr0608@gmail.com>
2024-06-20 18:37:03 +0900

    2024-06-20

--- programmers/GROUP BY/언어별 개발자 분류하기.sql
+++ programmers/GROUP BY/언어별 개발자 분류하기.sql
@@ -1,3 +1,8 @@
+/*
+GROUP_CONCAT(COLUMN_NAME)
+- 특정 그룹 내에서 여러 행의 값을 하나의 문자열로 연결
+- 기본적으로 쉼표로 구분 되며 GROUP_CONCAT(COLUMN_NAME SEPARATOR '|')와 같이 구분자 변경 가능
+*/
 SELECT  CASE
             WHEN GROUP_CONCAT(A.NAME) LIKE '%Python%' AND
                  GROUP_CONCAT(CATEGORY) LIKE '%Front End%' THEN 'A'
